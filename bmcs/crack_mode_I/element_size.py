03cd1fcc24d2163823aaf975cc0f33776871e879
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2017-05-30 14:22:56 +0200

    element_size with comparable parameters

--- bmcs/crack_mode_I/element_size.py
+++ bmcs/crack_mode_I/element_size.py
@@ -5,56 +5,55 @@ Created on May 23, 2017
 '''
 
 from scipy.optimize import newton
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-E = 1000.0
-eps_0 = 0.002
-eps_f = 0.02
+E = 34000.0
+f_t = 4.5
+G_f = 0.004
 
+eps_0 = f_t / E
+eps_f = G_f / f_t
+eps_max = 1.0 * eps_f
+eps = np.linspace(0, eps_max, 100)
 
-def w(eps): return 1 - ((eps_0 / eps) *
-                        np.exp(-(eps - eps_0) / (eps_f - eps_0)))
+# model with exponential softening law
+n_T = 100   # number of increments
+n_E = 100
+sig_t = np.zeros((n_T,), dtype=np.float)
+eps_t = np.zeros((n_T,), dtype=np.float)
 
-
-m = 100
-sigma = np.zeros((m,), dtype=np.float)
-eps_m = np.zeros((m,), dtype=np.float)
-
-for n in range(1, 11, 1):
-
-    sigma[0] = 0
-    eps_m[0] = 0
-
-    for i in range(1, m, 1):
-        sigma_i = E * eps_0 * (1 - i * 1.0 / m * 1.0)
-
-        def f_n(eps_s): return sigma_i - \
-            (eps_0 / eps_s) * \
-            np.exp(-(eps_s - eps_0) / (eps_f - eps_0)) * E * eps_s
-        eps_s = newton(f_n, eps_0, tol=1e-6, maxiter=50)
-
-        eps_m_i = (sigma_i / E) + (1. / n) * (eps_s - (sigma_i / E))
-
-        sigma[i] = sigma_i
-        eps_m[i] = eps_m_i
-
-    ax = plt.subplot(121)
-
-    ax.plot(eps_m, sigma, label='n=%i' % n)
+plt.subplot(111)
+n_E_list = range(10, n_E, 5)
+for n_e in n_E_list:  # n: number of elements
+    sig_t[0] = 0
+    eps_t[0] = 0
+    for i in range(1, n_T, 1):
+        # stress level starting from (ft=E*eps_0) until 0
+        sig_i = f_t * (1 - i * 1.0 / n_T * 1.0)
+        eps_s = eps_0 - (eps_f - eps_0) * np.log(sig_i / (eps_0 * E))
+        # calculating the average strain
+        L_e = 1. / n_e
+        eps_i = (sig_i / E) + L_e * (eps_s - (sig_i / E))
+        sig_t[i] = sig_i  # store the values of stress level
+        eps_t[i] = eps_i  # store the values of the average strain
+    plt.plot(eps_t, sig_t, label='n=%i' % n_e)
     plt.legend(loc=1)
-    ax.set_xlabel('strain')
-    ax.set_ylabel('stress')
 
-for n in range(1, 11, 1):
-    eps_m = (1. / n) * eps_f
-    x = np.array([0.0, eps_0, eps_m])
-    y = np.array([0.0, E * eps_0, 0.0])
+print eps_t[-1]
+plt.xlabel('strain')
+plt.ylabel('stress')
+plt.show()
 
+# model with linear softening law
+for n in range(1, n_E, 1):  # n: number of elements
+    eps_m = (1. / n) * eps_f  # calculating the average strain
+    # the strain values for plotting (we need only 3 values)
+    eps = np.array([0.0, eps_0, eps_m])
+    # the strain values for plotting (we need only 3 values)
+    sigma = np.array([0.0, E * eps_0, 0.0])
     ax = plt.subplot(122)
-    ax.plot(x, y, label='n=%i' % n)
-
+    ax.plot(eps, sigma, label='n=%i' % n)
     plt.legend(loc=1)
     ax.set_xlabel('strain')
     ax.set_ylabel('stress')
--- bmcs/crack_mode_I/softening_and_damage.py
+++ bmcs/crack_mode_I/softening_and_damage.py
@@ -2,6 +2,11 @@
 Created on May 23, 2017
 '''
 
+from bmcs.mats.mats_damage_fn import \
+    IDamageFn, LiDamageFn, JirasekDamageFn, AbaqusDamageFn,\
+    PlottableFn
+from traits.api import Float
+
 import numpy as np
 import pylab as p
 
@@ -23,34 +28,45 @@ def d_phi(eps_w, f_t, G_f):
 
 eps_0 = f_t / E
 eps_ch = G_f / f_t
-eps_max = 1.0 * eps_ch
+L = 1.0
+u_max = L * eps_ch
+eps_max = eps_ch
 eps = np.linspace(0, eps_max, 100)
 
-print 'eps_0', eps_0
+u_max = 0.00361632973221
 
 
-def g(eps, L_w):
-    g_eps = 1 - f_t * np.exp(-f_t * (eps - eps_0) * L_w / G_f) \
-        / (E * eps)
-    g_eps[np.where(eps - eps_0 < 0)] = 0
-    return g_eps
+class GfDamageFn(PlottableFn):
+
+    L_w = Float(1.0)
+
+    def __call__(self, eps):
+        L_w = self.L_w
+        g_eps = 1 - f_t * np.exp(-f_t * (eps - eps_0) * L_w / G_f) \
+            / (E * eps)
+        g_eps[np.where(eps - eps_0 < 0)] = 0
+        return g_eps
+
+    def diff(self, eps):
+        L_w = self.L_w
+        d_g_eps = (f_t * np.exp(L_w * (eps_0 - eps) * f_t / G_f)
+                   / (E * G_f * eps**2) *
+                   (G_f + L_w * eps * f_t))
+        d_g_eps[np.where(eps - eps_0 < 0)] = 0.0
+        return d_g_eps
 
 
-def d_g(eps, L_w):
-    d_g_eps = (f_t * np.exp(L_w * (eps_0 - eps) * f_t / G_f)
-               / (E * G_f * eps**2) *
-               (G_f + L_w * eps * f_t))
-    d_g_eps[np.where(eps - eps_0 < 0)] = 0.0
-    return d_g_eps
+omega_fn = LiDamageFn(alpha_1=1.0, alpha_2=20000, s_0=eps_0)
+#omega_fn = JirasekDamageFn(s_0=eps_0)
+omega_fn = GfDamageFn()
+# p.plot(eps, omega_fn(eps))
+# p.plot(eps, omega_fn_gf(eps))
+# p.show()
 
+n_T = 500
+K_max = 1000
 
-n_T = 100
-u_max = 0.004
-
-L = 1.0
-N = 30.0
-
-for N in [10, 20, 30]:
+for N in [1.00000001, 2, 3, 4, 6, 7]:
     sig_t = []
     eps_t = []
     L_el = (N - 1.0) / N * L
@@ -58,21 +74,21 @@ for N in [10, 20, 30]:
     eps_w = 0.00001
     eps_w_arr = np.array([eps_w], dtype=np.float_)
     u_t = np.linspace(0, u_max, n_T)
-    h_e = L_w
+    omega_fn.L_w = L_w
     for u in u_t:
-        for K in range(100):
+        for K in range(K_max):
             R = 1 / L_el * (u - eps_w_arr * L_w) - \
-                (1 - g(eps_w_arr, h_e)) * eps_w_arr
+                (1 - omega_fn(eps_w_arr)) * eps_w_arr
             if np.fabs(R) < 1e-8:
-                print 'converged in ', K
+                print 'step %g converged in %d steps' % (u, K)
                 break
-            dR = -L_w / L_el + d_g(eps_w_arr, h_e) * \
-                eps_w_arr - (1 - g(eps_w_arr, h_e))
+            dR = -L_w / L_el + omega_fn.diff(eps_w_arr) * \
+                eps_w_arr - (1 - omega_fn(eps_w_arr))
             d_eps_w = -R / dR
             eps_w_arr += d_eps_w
-        if K == 99:
+        if K == K_max - 2:
             raise ValueError, 'No convergence'
-        sig = ((1.0 - g(eps_w_arr, L_w)) * E * eps_w_arr)[0]
+        sig = ((1.0 - omega_fn(eps_w_arr)) * E * eps_w_arr)[0]
         sig_t.append(sig)
         eps_t.append(u / L)
 
