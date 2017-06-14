480d3107dea09aa62e61f33211ad5d06014f4a95
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2017-06-14 13:37:03 +0200

    minor change

--- ibvpy/mats/mats3D/mats3D_plastic/yield_faceJ2.py
+++ ibvpy/mats/mats3D/mats3D_plastic/yield_faceJ2.py
@@ -3,27 +3,30 @@ Created on 14.06.2017
 
 @author: Yingxiong
 '''
+import mayavi.mlab as m
 import numpy as np
-
-min_sig = -20.0
-max_sig = 5.0
-n_sig = 100j
+# define the boundaries of a cube in stress space
+min_sig = -20.0  # maximum compression
+max_sig = 5.0  # maximum tension
+n_sig = 100j  # number of values in each stress direction
 sig_1, sig_2, sig_3 = np.mgrid[min_sig: max_sig: n_sig,
                                min_sig: max_sig: n_sig,
                                min_sig: max_sig: n_sig]
-
+# make a four dimensional array
 sig_abcj = np.einsum('jabc->abcj', np.array([sig_1, sig_2, sig_3]))
+# Kronecker delta
 DELTA = np.identity(3)
 sig_abcij = np.einsum('abcj,jl->abcjl', sig_abcj, DELTA)
-
+# first invariant of the stress tensor
 I1 = np.einsum('...ii,...ii', sig_abcij, DELTA)
+# deviatoric stress tensor in each point
 s_ij = sig_abcij - np.einsum('...,ij->...ij', I1 / 3.0, DELTA)
+# second deviator of the stress tensor
 J2 = np.einsum('...ii,...ii', s_ij, s_ij) / 2.0
-
+# threshold defining the radial distance from hydrostatic axis
 k = 2.
 f = J2 - k ** 2
-
-import mayavi.mlab as m
+# visualization using Mayavi package
 f_pipe = m.contour3d(
     sig_1, sig_2, sig_3, f, contours=[0.0], color=(0, 1, 0))
 m.axes(f_pipe)
