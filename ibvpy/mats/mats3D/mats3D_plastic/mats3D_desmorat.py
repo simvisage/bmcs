ecd923bb8a5c4e466e3b02a4067757537700663d
Abedulgader Baktheer <abedulgader.baktheer@rub.de>
2018-12-06 11:02:07 +0100

    Desmorat model 3D

--- ibvpy/mats/mats3D/mats3D_plastic/mats3D_desmorat.py
+++ ibvpy/mats/mats3D/mats3D_plastic/mats3D_desmorat.py
@@ -36,60 +36,60 @@ class MATS3DDesmorat(MATS3DEval, MATS3D):
     # Material parameters
     #-------------------------------------------------------------------------
 
-    E_m = tr.Float(16e+3,
-                   label="E_m",
-                   desc="Young's Modulus",
+    E_1 = tr.Float(15e+3,
+                   label="E_1",
+                   desc="first Young's Modulus",
                    auto_set=False,
                    input=True)
-    E_b = tr.Float(19e+3,
-                   label="E_b",
-                   desc="Young's Modulus Bond",
+    E_2 = tr.Float(19e+3,
+                   label="E_2",
+                   desc="second Young's Modulus",
                    auto_set=False,
                    input=True)
 
     nu = tr.Float(0.2,
                   label='nu',
-                  desc="Poison ratio",
+                  desc="Poisson ratio",
                   auto_set=False,
                   input=True)
 
-    def _get_lame_m_params(self):
-        la = self.E_m * self.nu / ((1. + self.nu) * (1. - 2. * self.nu))
+    def _get_lame_1_params(self):
+        la = self.E_2 * self.nu / ((1. + self.nu) * (1. - 2. * self.nu))
         # second Lame parameter (shear modulus)
-        mu = self.E_m / (2. + 2. * self.nu)
+        mu = self.E_2 / (2. + 2. * self.nu)
         return la, mu
 
-    D_m_abef = tr.Property(tr.Array, depends_on='+input')
+    D_1_abef = tr.Property(tr.Array, depends_on='+input')
 
     @tr.cached_property
-    def _get_D_m_abef(self):
-        la = self._get_lame_m_params()[0]
-        mu = self._get_lame_m_params()[1]
+    def _get_D_1_abef(self):
+        la = self._get_lame_1_params()[0]
+        mu = self._get_lame_1_params()[1]
         delta = np.identity(3)
-        D_m_abef = (np.einsum(',ij,kl->ijkl', la, delta, delta) +
+        D_1_abef = (np.einsum(',ij,kl->ijkl', la, delta, delta) +
                     np.einsum(',ik,jl->ijkl', mu, delta, delta) +
                     np.einsum(',il,jk->ijkl', mu, delta, delta))
 
-        return D_m_abef
+        return D_1_abef
 
-    def _get_lame_b_params(self):
-        la = self.E_b * self.nu / ((1. + self.nu) * (1. - 2. * self.nu))
+    def _get_lame_2_params(self):
+        la = self.E_2 * self.nu / ((1. + self.nu) * (1. - 2. * self.nu))
         # second Lame parameter (shear modulus)
-        mu = self.E_b / (2. + 2. * self.nu)
+        mu = self.E_2 / (2. + 2. * self.nu)
         return la, mu
 
-    D_b_abef = tr.Property(tr.Array, depends_on='+input')
+    D_2_abef = tr.Property(tr.Array, depends_on='+input')
 
     @tr.cached_property
-    def _get_D_b_abef(self):
-        la = self._get_lame_b_params()[0]
-        mu = self._get_lame_b_params()[1]
+    def _get_D_2_abef(self):
+        la = self._get_lame_2_params()[0]
+        mu = self._get_lame_2_params()[1]
         delta = np.identity(3)
-        D_b_abef = (np.einsum(',ij,kl->ijkl', la, delta, delta) +
+        D_2_abef = (np.einsum(',ij,kl->ijkl', la, delta, delta) +
                     np.einsum(',ik,jl->ijkl', mu, delta, delta) +
                     np.einsum(',il,jk->ijkl', mu, delta, delta))
 
-        return D_b_abef
+        return D_2_abef
 
     tree_node_list = List([])
 
@@ -111,7 +111,7 @@ class MATS3DDesmorat(MATS3DEval, MATS3D):
               enter_set=True,
               auto_set=False)
 
-    S = Float(476,
+    S = Float(476e-6,
               label="S",
               desc="damage strength",
               MAT=True,
@@ -129,13 +129,13 @@ class MATS3DDesmorat(MATS3DEval, MATS3D):
                     enter_set=True,
                     auto_set=False)
 
-    state_array_shapes = {'tau_e': (3, 3),
-                          'tau': (3, 3),
-                          'eps_pi': (3, 3),
-                          'X': (3, 3),
-                          'z': (),
-                          'omega': (),
-                          'Y': ()}
+    state_array_shapes = {'sigma_Emab': (3, 3),
+                          'sigma_pi_Emab': (3, 3),
+                          'eps_pi_Emab': (3, 3),
+                          'X_Emab': (3, 3),
+                          'z_Ema': (),
+                          'omega_Ema': (),
+                          'Y_Ema': ()}
     r'''
     Shapes of the state variables
     to be stored in the global array at the level 
@@ -144,115 +144,57 @@ class MATS3DDesmorat(MATS3DEval, MATS3D):
 
     def get_corr_pred(self, eps_Emab_n1, deps_Emab, tn, tn1,
                       update_state, algorithmic,
-                      tau_e, tau, eps_pi, X, z, omega, Y):
+                      sigma_Emab, sigma_pi_Emab, eps_pi_Emab, X_Emab, z_Ema, omega_Ema, Y_Ema):
         r'''
         Corrector predictor computation.
         '''
         if update_state:
             eps_Emab_n = eps_Emab_n1 - deps_Emab
 
-        sigma_Emab = np.einsum(
-            '...abcd,...cd->...ab', self.D_abef, eps_Emab_n1
-        )
-        return self.D_abef, sigma_Emab
+            Em_len = len(eps_Emab_n1.shape) - 2
+            new_shape = tuple([1 for i in range(Em_len)]) + self.D_abef.shape
+            D_1_abef = self.D_1_abef.reshape(*new_shape)
+            D_2_abef = self.D_2_abef.reshape(*new_shape)
 
-    def get_next_step(self, eps, eps_pi, Y, D, z, X, tau_e, tau, g):
+            print D_2_abef
 
-        D_m = self.D_m_abef
-        D_b = self.D_b_abef
-        #v = len(s) - 1
+            D_m = self.D_1_abef
+            D_b = self.D_2_abef
 
-        for i in range(len(s_levels) - 1):
+            sigma_pi_Emab_trial = (
+                np.einsum('...ijkl,...kl->...ij', D_2_abef, eps_Emab_n))
 
-            # trial stress - assuming elastic increment.
-            eps_diff = eps[i + 1] - eps_pi[i]
-            bn = (1. - D[i])
-            tau_trial = (np.einsum('ijkl,lk->ij', D_m, eps[i + 1]) * (1. - D[i]) +
-                         np.einsum('ijkl,lk->ij', D_b, eps_diff)) * (1. - D[i])
-            tau_e_trial = (np.einsum('ijkl,lk->ij', D_b, eps_diff))
-            norm = tau_e_trial - X[i]
-            f_trial = np.sqrt(np.einsum('nj,nj', norm, norm)
-                              ) - self.tau_bar - self.K * z[i]
+            norm = sigma_pi_Emab_trial - X_Emab,
+            f = np.sqrt(np.einsum('Emnj,Emnj', norm, norm)
+                        ) - self.tau_bar - self.K * z_Ema
 
-            if f_trial <= 0:
-                tau_e[i + 1] = tau_e_trial
-                tau[i + 1] = tau_trial
-                eps_pi[i + 1] = eps_pi[i]
-                z[i + 1] = z[i]
-                X[i + 1] = X[i]
-                D[i + 1] = D[i]
-                Y[i + 1] = Y[i]
+            plas_1 = f > 1e-6
+            elas_1 = f < 1e-6
 
-            # identify values beyond the elastic limit
-            else:
-                # identify values beyond the elastic limit
+            delta_pi = f / \
+                (self.E_2 + (self.K + self.gamma) * (1. - omega_Ema)) * plas_1
 
-                # sliding  multiplier
-                delta_pi = f_trial / \
-                    (self.E_b + (self.K + self.gamma) * (1. - D[i]))
+            norm2 = sigma_pi_Emab_trial - X_Emab
+            norm3 = 1.0 * elas_1 + \
+                np.sqrt(np.einsum('...ab,...ab', norm, norm)) * plas_1
 
-         # return mapping for isotropic and kinematic hardening
-                #grad_f = np.sign(tau_e_trial - X[i])
-                norm2 = tau_e[i] - X[i]
-                norm3 = np.sqrt(np.einsum('nj,nj', norm, norm))
-                eps_pi[i + 1] = eps_pi[i] + norm2 * delta_pi / norm3
-                Y[i + 1] = 0.5 * (np.einsum('ij,ijkl,lk', eps[i + 1], D_m, eps[i + 1])) + \
-                    0.5 * (np.einsum('ij,ijkl,lk', eps_diff, D_b, eps_diff))
-                D_trial = D[i] + (Y[i + 1] / self.S) * delta_pi  # * 1e6
-                if D_trial > 1.0:
-                    D[i + 1] = 1.0
-                else:
-                    D[i + 1] = D_trial
-                g[i + 1] = g[i] + (1. - D[i + 1]) * (eps_pi[i + 1] - eps_pi[i])
-                X[i + 1] = X[i] + self.gamma * g[i + 1]
-                z[i + 1] = z[i] + delta_pi * (1. - D[i + 1])
+            eps_pi_Emab = eps_pi_Emab + plas_1 * (norm2 * delta_pi / norm3)
 
-                # apply damage law to the effective stress
-                eps_diff = eps[i + 1] - eps_pi[i + 1]
-                tau[i + 1] = (np.einsum('ijkl,lk->ij', D_m, eps[i + 1]) * (1. - D[i + 1]) + (np.einsum('ijkl,lk->ij',
-                                                                                                       D_m, eps[i + 1])) * (1. - D[i + 1]) - np.einsum('ijkl,lk->ij', D_b, eps_pi[i + 1])) * (1. - D[i + 1])
-                tau_e[i + 1] = (np.einsum('ijkl,lk->ij', D_b, eps_diff))
-        return eps_pi, Y, D, z, X, tau_e, tau, g
+            eps_diff_Emab = eps_Emab_n - eps_pi_Emab
 
-    traits_view = View(
-        Group(
-            VGroup(
-                Item('E_b', full_size=True, resizable=True),
-                Item('E_m'),
-                Item('gamma'),
-                Item('K'),
-                Item('S'),
-                Item('tau_bar'),
-            ),
+            Y_Ema = 0.5 * (np.einsum('...ij,...ijkl,...kl', eps_Emab_n, D_m, eps_Emab_n)) + \
+                0.5 * (np.einsum('...ij,...ijkl,...kl',
+                                 eps_diff_Emab, D_b, eps_diff_Emab))
 
-        ),
-        width=0.4,
-        height=0.8,
-    )
+            omega_Ema = omega_Ema + (Y_Ema / self.S) * delta_pi
 
-    tree_view = traits_view
+            sigma_Emab = ((1.0 - omega_Ema) * np.einsum('...ijkl,...kl->...ij', D_1_abef, eps_Emab_n) +
+                          (1.0 - omega_Ema) * np.einsum('...ijkl,...kl->...ij', D_2_abef, eps_Emab_n))
 
+            phi_Emn = 1.0 - omega_Ema
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    m = MATS3DDesmorat()
-    s_levels = np.linspace(0, -0.0035, 10)
+            D_abef = (D_1_abef + D_2_abef)
+            D_abef = np.einsum('...a, ...ijkl->...ijkl',
+                               phi_Emn,  D_1_abef + D_2_abef)
 
-    eps = np.array([np.zeros((3, 3)) for _ in range(len(s_levels))])
-
-    for i in range(len(s_levels)):
-        eps[i][0][0] = s_levels[i]
-
-    eps_pi = np.array([np.zeros((3, 3)) for _ in range(len(s_levels))])
-    X = np.array([np.zeros((3, 3)) for _ in range(len(s_levels))])
-    g = np.array([np.zeros((3, 3)) for _ in range(len(s_levels))])
-    tau_e = np.array([np.zeros((3, 3)) for _ in range(len(s_levels))])
-    tau = np.array([np.zeros((3, 3)) for _ in range(len(s_levels))])
-    Y = np.zeros_like(s_levels)
-    D = np.zeros_like(s_levels)
-    z = np.zeros_like(s_levels)
-    eps_pi, Y, D, z, X, tau_e, tau, g = m.get_next_state(
-        eps, eps_pi, Y, D, z, X, tau_e, tau, g)
-    plt.plot(eps[:, 0, 0], tau[:, 0, 0])
-    plt.show()
-#   m.configure_traits()
+        return D_abef, sigma_Emab
