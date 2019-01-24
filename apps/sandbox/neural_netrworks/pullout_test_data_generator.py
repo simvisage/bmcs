27f5d50a1624772d9e2706ad69fa61516052c0aa
Unknown <pata.benkova@gmail.com>
2019-01-24 15:08:45 +0100

    Auto stash before merge of "python3" and "origin/python3"

--- apps/sandbox/neural_netrworks/pullout_test_data_generator.py
+++ apps/sandbox/neural_netrworks/pullout_test_data_generator.py
@@ -11,11 +11,12 @@ from bmcs.pullout.pullout_multilinear import \
     PullOutModel
 import numpy as np
 import pylab as p
+from fontTools.cffLib import CFFFontSet
 
 
 home_dir = os.path.expanduser('~')
 data_dir = os.path.join(home_dir, 'data')
-f_name = 'trainng_data.dat'
+f_name = 'trainng_data.txt'
 file_path = os.path.join(data_dir, f_name)
 print('data stored in', file_path)
 if not os.path.exists(data_dir):
@@ -28,38 +29,47 @@ if __name__ == '__main__':
     po.geometry.L_x = 200.0
     po.loading_scenario.set(loading_type='monotonic')
 
-    w_at_max = []
+    
     L_list = [10, 20, 50, 100]
     A_m_list = np.array(
         [10.0 * distance
          for distance in[10., 20., 30., 40]],
         dtype=np.float_
     )
+    A_f_list = np.array([0.5, 1.0, 1.5, 2.0], dtype=np.float_)
+    tau_list = np.array(['3.0, 4.0, 5.0, 6.0, 7.0, 8.0'])
+    s_list = np.array(['0.1, 0.2, 0.3, 0.4, 0.5, 0.6'])
+    
+    
+    P_max = []
+    w_at_max = []
+    
     for L in L_list:
-        P_max = []
         for A_m in A_m_list:
-            print('',)
-            po.geometry.L_x = L
-            po.cross_section.set(A_f=16.67 / 9.0, P_b=1.0, A_m=A_m)
-            po.mats_eval.set(s_data='0, 0.1, 0.4, 1.7',
-                             tau_data='0, 70, 0, 0')
-            po.mats_eval.update_bs_law = True
-
-            po.run()
-
-            P_t = po.get_P_t()
-            w_0, w_L = po.get_w_t()
-
-    #        p.plot(w_L, P_t)
-            m_idx = np.argmax(P_t)
-            P_max.append(P_t[m_idx])
-            w_at_max.append(w_L[m_idx])
-
-    #         print(P_max, w_at_max)
-    #         p.plot(w_L, P_t)
-        p.plot(A_m_list, P_max)
+            for A_f in A_f_list:   # I assumed ring cross-section
+                for tau in tau_list:
+                    for s in s_list: 
+                        
+                        print('',)
+                        d_f = 2*((A_f/np.pi)**0.5)    # diameter
+                        po.geometry.L_x = L
+                        po.cross_section.set(A_f = A_f, P_b = ((np.pi)*(d_f)), A_m = A_m)
+                        po.mats_eval.set(s_data = s,
+                             tau_data = tau)
+                        po.mats_eval.update_bs_law = True
+                        po.run()
+                        
+                        P_t = po.get_P_t()
+                        w_0, w_L = po.get_w_t()
+                        m_idx = np.argmax(P_t)
+                        P_max.append(P_t[m_idx])
+                        w_at_max.append(w_L[m_idx])
+                
+      
+    # print(P_max)
 #    p.plot(L_list, P_max)
     p.show()
 print("L_list=",L_list,'\n',"P_max=",P_max)
+print(len(P_max))
 np.savetxt(file_path, P_max)
 
