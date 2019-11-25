545d8844f8555abb1c0b9a00a42c7225d81789ae
Fahad Seemab <fahadseemab@outlook.com>
2019-11-25 16:28:29 +0100

    Updated model with an attempt of implementation in Mayavi
    The three scripts added contains an updated material model along with updated example to see implementation on mayavi framework

--- apps/sandbox/fahad/norm_force_axisym.py
+++ apps/sandbox/fahad/norm_force_axisym.py
@@ -1,84 +1,134 @@
 '''
-Created on 30.07.2019
+Created on 01.10.2019
 
 @author: fseemab
 '''
-import time
+import os.path
 
-from ibvpy.fets import FETS2D4Q
-from simulator.xdomain.xdomain_fe_grid import XDomainFEGrid
-from view.window.bmcs_window import BMCSWindow
+from ibvpy.mats.viz3d_scalar_field import \
+    Vis3DStateField, Viz3DScalarField
+from ibvpy.mats.viz3d_tensor_field import \
+    Vis3DTensorField, Viz3DTensorField
+from mayavi import mlab
+from simulator.demo.mlab_decorators import decorate_figure
 
-from apps.sandbox.fahad.pullout_curves_normforce import verify02_quasi_pullout
-from apps.verify.bond_cum_damage.pullout_2d_model.pullout2d_model import PullOut2D
-import matplotlib.pyplot as plt
+from apps.sandbox.fahad.verify_sorting.pulloutaxisymdemomodel import \
+    PullOutAxiSym, Geometry, CrossSection
 import numpy as np
 import pylab as p
-import traits.api as tr
+
+dir = os.path.expanduser('~')
 
 
 def verify_normalized_pullout_force():
 
-    ds = 16
-    r_steel = ds / 2
-    L_x = ds * 5
-    r_concrete = 75
-    n_x = 2
-    n_y = 2
     ax = p.subplot(111)
 
-    f_list = [-5]
-    for f_lateral in f_list:  # [0, -100]
-
+    f_lateral = 0
+    dt_list = [0.4]
+    for dt in dt_list:  # [0, -100]
+        ds = 16
         print('lateral confining pressure', f_lateral)
-
-        s = verify02_quasi_pullout(f_lateral=f_lateral)
+        g = Geometry(L_x=ds * 5)
+        c = CrossSection(R_m=75
+                         ,
+                         R_f=ds / 2)
+        s = PullOutAxiSym(geometry=g,
+                          cross_section=c,
+                          n_x=5,
+                          n_y_concrete=1,
+                          n_y_steel=1)
+        s.tloop.k_max = 1000
+        s.f_lateral = f_lateral
         s.xd_steel.trait_set(coord_min=(0, 0),
-                             coord_max=(L_x, r_steel),
-                             shape=(n_x, 1)
+                             coord_max=(g.L_x, c.R_f),
+                             shape=(s.n_x, s.n_y_steel)
                              )
-        s.xd_concrete.trait_set(coord_min=(0, r_steel),
-                                coord_max=(r_steel, r_concrete),
-                                shape=(n_x, n_y)
+        s.xd_concrete.trait_set(coord_min=(0, c.R_f),
+                                coord_max=(g.L_x,
+                                           c.R_m),
+                                shape=(s.n_x, s.n_y_concrete)
                                 )
-        s.u_max = 0.5
-        s.tline.step = 0.1
-        s.run()
-        print('F', np.sum(s.hist.F_t[-1, s.right_x_s.dofs]))
-
-        print('P_max', np.max(s.record['Pw'].sim.hist.F_t))
-        print('P_end', np.sum(s.hist.F_t[-1, s.right_x_s.dofs]))
-        w = s.get_window()
-        w.viz_sheet.viz2d_dict['Pw'].plot(ax, 1)
-#         if False:
-#             s = verify02_quasi_pullout(f_lateral=f_lateral)
-#             s.xd_steel.trait_set(coord_min=(0, 0),
-#                                  coord_max=(L_x, r_steel),
-#                                  shape=(n_x, 1)
-#                                  )
-#             s.xd_concrete.trait_set(coord_min=(0, r_steel),
-#                                     coord_max=(r_steel, r_concrete),
-#                                     shape=(n_x, n_y)
-#                                     )
-#             s.u_max = 0.5
-#             s.tline.step = 0.05
         s.m_steel.trait_set(E=200000, nu=0.3)
         s.m_concrete.trait_set(E=29800, nu=0.3)
         s.m_ifc.trait_set(E_T=12900,
-                          E_N=1e9,
+                          E_N=1e5,
                           tau_bar=4.2,  # 4.0,
-                          K=0, gamma=10,  # 10,
-                          c=1, S=0.0025, r=1,
-                          m=0,
-                          algorithmic=False)
-        s.f_lateral = f_lateral
+                          K=11, gamma=55,  # 10,
+                          c=2.8, S=0.00048, r=0.51,
+                          m=0.175,  # 0.175,
+                          #                           algorithmic=False)
+                          )
 
+        s.u_max = 0.1
+        s.tline.step = dt
+        s.tloop.verbose = True
+        s.run()
+
+        print('P_max', np.max(s.record['Pw'].sim.hist.F_t))
+        print('P_end', np.sum(s.hist.F_t[-1, s.right_x_s.dofs]))
+        
+        #=======================================================================
+        # mlab.options.backend = 'envisage'
+        # print(s.hist['stress'])
+        # f_stress = mlab.figure()
+        # scene = mlab.get_engine().scenes[-1]
+        # scene.name = 'stress'
+        # stress_viz = Viz3DTensorField(vis3d=s.hist['stress'])
+        # stress_viz.setup()
+        # stress_viz.warp_vector.filter.scale_factor = 100.0
+        # stress_viz.plot(s.tstep.t_n)
+        # lut_manager = mlab.colorbar(title='sig_ab', orientation='horizontal', nb_labels=5)
+        # # fix the range
+        # print(s.record['stress'])
+        # # scalar_lut_manager.data_range = array([0., 1.])
+        # lut_manager.data_range = np.array([np.min(s.record['stress']), np.max(s.record['stress'])])
+        #=======================================================================
+        
+        #=======================================================================
+        # f_strain = mlab.figure()
+        # scene = mlab.get_engine().scenes[-1]
+        # scene.name = 'strain'
+        # strain_viz = Viz3DTensorField(vis3d=s.hist['strain'])
+        # strain_vis = Vis3DTensorField(vis=s.hist['strain'])
+        # strain_viz.setup()
+        # strain_viz.warp_vector.filter.scale_factor = 100.0
+        # strain_viz.plot(s.tstep.t_n)
+        # lut_manager = mlab.colorbar(title='eps_ab', orientation='horizontal', nb_labels=5)
+        # # fix the range
+        # lut_manager.data_range = (np.min(s.record['strain']), np.max(s.record['strain']))
+        #=======================================================================
+        #=======================================================================
+        # 
+        # f_damage = mlab.figure()
+        # scene = mlab.get_engine().scenes[-1]
+        # scene.name = 'damage'
+        # damage_viz = Viz3DScalarField(vis3d=s.hist['damage'])
+        # damage_viz.setup()
+        # damage_viz.lut_manager.use_default_range = True
+        # damage_viz.warp_vector.filter.scale_factor = 10.0
+        # damage_viz.plot(s.tstep.t_n)
+        # damage_viz.plot(0.0)
+        #=======================================================================
+
+        # decorate_figure(f_stress, stress_viz)  # , 800, [300, 40, 0])
+        # decorate_figure(f_strain, strain_viz)  # , 800, [300, 40, 0])
+        #=======================================================================
+        # decorate_figure(f_damage, damage_viz, 800, [300, 40, 0])
+        #=======================================================================
+        
+        s.f_lateral = f_lateral
+        
         w = s.get_window()
         w.viz_sheet.viz2d_dict['Pw'].plot(ax, 1)
-
-        s.run()
-    p.show()
+    return w
 
 
 if __name__ == '__main__':
-    verify_normalized_pullout_force()
+    #     abc = open('sigNnew1.txt', 'w')
+    #     abc.close()
+    w = verify_normalized_pullout_force()
+    w.configure_traits()
+    # p.show()
+    # mlab.show()
+    
